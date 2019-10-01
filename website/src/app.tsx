e8fb970601c44e73759baf7f8a75e1756000d836
Alex Higgins <thealexanderhiggins@gmail.com>
2019-09-30 21:01:43 -0400

    Styled website

--- website/src/app.tsx
+++ website/src/app.tsx
@@ -11,40 +11,91 @@ function App() {
   const [evalResult, setEvalResult] = useState("");
 
   return (
-    <div className="App" style={{ display: "flex" }}>
-      <div style={{ display: "flex", flexDirection: "column" }}>
-        <textarea
-          style={{ width: "400px", height: "500px" }}
-          value={input}
-          onChange={e => {
-            setInput(e.target.value);
-          }}
-        ></textarea>
-        <button
-          onClick={() => {
-            const l = new Lexer(input);
-            const p = new Parser(l);
-            const program = p.parseProgram();
-            if (p.errors.length) {
-              setEvalResult(JSON.stringify({ errors: p.errors }, null, 2));
-            } else {
-              const result = evalNode(program, new Environment());
-              if (result) {
-                setEvalResult(result.inpect());
-              } else {
-                setEvalResult("<no result>");
-              }
-            }
-          }}
-        >
-          Run
-        </button>
-      </div>
+    <div style={{ backgroundColor: "#333333", height: "100vh" }}>
+      <a
+        href="https://github.com/Aedalus/cacti-script"
+        style={{ position: "absolute", top: 0, right: 0 }}
+      >
+        <img
+          width="149"
+          height="149"
+          src="https://github.blog/wp-content/uploads/2008/12/forkme_right_white_ffffff.png?resize=149%2C149"
+          className="attachment-full size-full"
+          alt="Fork me on GitHub"
+          data-recalc-dims="1"
+        ></img>
+      </a>
+      <h1
+        style={{
+          color: "white",
+          fontFamily: "lobster",
+          fontSize: "4em",
+          paddingLeft: "20px",
+          paddingTop: "20px"
+        }}
+      >
+        Cacti Script
+      </h1>
+      <h3
+        style={{
+          color: "white",
+          fontFamily: "lobster",
+          paddingLeft: "20px",
+          marginTop: "10px"
+        }}
+      >
+        A light scripting language ready to run everywhere*
+      </h3>
 
-      <textarea
-        style={{ width: "400px", height: "500px" }}
-        value={evalResult}
-      ></textarea>
+      <div className="App" style={{ display: "flex", padding: "20px" }}>
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <textarea
+            style={{
+              width: "400px",
+              height: "500px",
+              fontFamily: "monospace",
+              backgroundColor: "#222222",
+              color: "#DDDDDD"
+            }}
+            value={input}
+            onChange={e => {
+              setInput(e.target.value);
+            }}
+          ></textarea>
+          <button
+            onClick={() => {
+              const l = new Lexer(input);
+              const p = new Parser(l);
+              const program = p.parseProgram();
+              if (p.errors.length) {
+                setEvalResult(JSON.stringify({ errors: p.errors }, null, 2));
+              } else {
+                const result = evalNode(program, new Environment());
+                if (result) {
+                  setEvalResult(result.inspect());
+                } else {
+                  setEvalResult("<no result>");
+                }
+              }
+            }}
+          >
+            Run
+          </button>
+        </div>
+        <textarea
+          style={{
+            width: "400px",
+            height: "500px",
+            marginLeft: "20px",
+            backgroundColor: "#EEEEEE"
+          }}
+          value={evalResult}
+        ></textarea>
+      </div>
+      <p style={{ color: "white", paddingLeft: "20px", fontFamily: "roboto" }}>
+        *Please do not run Cacti Script anywhere, especially any staging or
+        production environments.
+      </p>
     </div>
   );
 }
